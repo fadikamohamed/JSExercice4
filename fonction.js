af903ef8bbe41d0e59f5ba60530f0afa9c2c92c7
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-21 10:25:11 +0200

    Ajout des commentaires

--- fonction.js
+++ fonction.js
@@ -1,13 +1,17 @@
+//Déclaration de la fonction.
 function identity() {
-
+ //Déclaration des variables.
   var lastname = document.getElementById('lastname').value;
   var firstname = document.getElementById('firstname').value;
   var city = document.getElementById('city').value;
+  //Déclaration du gérex et intégation dans une variable.
   var regex = /^[a-zA-Z àáâãäåçèéêëìíîïðòóôõöùúûüýÿ\-']+$/;
-
+  //Déclaration de la condition.
   if ((regex.test(lastname) == true) && (regex.test(lastname) == true) && (regex.test(lastname) == true)) {
+    //Boite de dialogue avec appel des variables.
     alert('Nom : ' + lastname + '\nPrénom : ' + firstname + '\nVille : ' + city);
   }else {
+    //Boite de dialogue avec message d'érrur.
     alert('Mauvaise saisie !');
   }
 }
